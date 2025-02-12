02bf00ae2430a07fc063b7512d18c38e29f0aaa2
Marcelo Almeida <marcelo.aallmeida@gmail.com>
2025-02-12 15:50:46 -0300

    build: project settings and build options

---
+++ frontend/cypress.config.ts
@@ -0,0 +1,15 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:5173",
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+  },
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+    specPattern: "cypress/component/**/*.cy.{js,jsx,ts,tsx}",
+  },
+});
